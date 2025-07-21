acdebe1c031fc5f53b5d5fd700169d97292bf951
Emma37059 <leonaremma@myvuw.ac.nz>
2025-07-22 11:40:43 +1200

    PBA
    worked out colour, did PBA

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,11 +1,16 @@
 //your parameter variables go here!
+//middle box size
 let rect_width  = 20;
-let rect_height = 30;
+let rect_height = 20;
+//ellipse size
+let ellipse_width = 18;
+let ellipse_height = 18;
+
 
 
 function setup_wallpaper(pWallpaper) {
-  //pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
+  //pWallpaper.output_mode(GRID_WALLPAPER);
   //swap these two out for the different tile modes
   //choose glide or glyph (standard) modes
   
@@ -16,16 +21,49 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
   //flexibility to change this
-  pWallpaper.grid_settings.row_offset  = 0;
+  pWallpaper.grid_settings.row_offset  = 50;
   //horizontally moves tiles like bricks or on top of each other
 }
 
 function wallpaper_background() {
-  background(204, 244, 252); //light honeydew green colour
+  background(204, 244, 252); //colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  //tesing
+
+//colour has to be on top of parameter!!!!!!!
+
+  stroke(252, 162, 176); //RGB pink
+  line(1, 1, 200, 200); //top left
+  line(200, 1, 1, 200); //top right
+
+  //middle box
+  stroke(252, 162, 176); //RGB pink
+  fill(204, 244, 252);
+  rect(90 , 90, rect_width, rect_height);
+
+  stroke(15, 215, 255); //RGB value
+  fill(15, 215, 255);
+  ellipse(100, 100, ellipse_width, ellipse_height);
+
+  //stroke(252, 162, 176); //RGB pink
+  //line(50, 50, 50, 150);
+  //line(50, 150, 150, 150);
+  //line(150, 150, 150, 50);
+  //line(150, 50, 50, 50);
+
+// across, down/up, across, downup
+}
+
+
+
+
+
+
+
+
+
+  //testing
   //rect(40 ,40, rect_width, rect_height);
   //ellipse(20 , 20, 50, 50);
   //rect(120, 120, 30, 20);
@@ -35,16 +73,4 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   //!!line parameters... first point x/across, y/vertical, second point x/across, y/vertical
   //!!further down in code, is on top, first line of code is at the bottom
   
-  line(50, 50, 50, 150);
-  line(50, 150, 150, 150);
-  line(150, 150, 150, 50);
-  line(150, 50, 50, 50);
-
-  ellipse(100, 100, 100, 100);
-
-  line(1, 1, 50, 50);
-  line(200, 200, 150, 150);
-  line(1, 1, 50, 150);
-  //work left to right!
-  line(150, 50, 200, 200);
-}
+  
