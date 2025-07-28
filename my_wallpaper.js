a04a537e2f4be2f15749af22554b54968cfafe28
Emma37059 <leonaremma@myvuw.ac.nz>
2025-07-29 11:06:13 +1200

    trying to make sense of if statements

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -6,7 +6,8 @@ let rect_height = 20;
 let ellipse_width = 18;
 let ellipse_height = 18;
 
-
+let x_example = 13;
+let y_example = 7;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -30,21 +31,46 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+//above is my draw function but it doesn't clear so doesn't reset
+
+//
+
+rect (20, 20, x_example, y_example);
+
+// across, down/up, across, downup
+
+if(y_example > 5){
+//because the let=y is bigger than 5 it changes the let=x to 12
+  x = 12
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //colour has to be on top of parameter!!!!!!!
 
-  stroke(252, 162, 176); //RGB pink
-  line(1, 1, 200, 200); //top left
-  line(200, 1, 1, 200); //top right
+  //stroke(252, 162, 176); //RGB pink
+  //line(1, 1, 200, 200); //top left
+  //line(200, 1, 1, 200); //top right
 
   //middle box
-  stroke(252, 162, 176); //RGB pink
-  fill(204, 244, 252);
-  rect(90 , 90, rect_width, rect_height);
+  //stroke(252, 162, 176); //RGB pink
+  //fill(204, 244, 252);
+  //rect(90 , 90, rect_width, rect_height);
 
-  stroke(15, 215, 255); //RGB value
-  fill(15, 215, 255);
-  ellipse(100, 100, ellipse_width, ellipse_height);
+  //stroke(15, 215, 255); //RGB value
+  //fill(15, 215, 255);
+  //ellipse(100, 100, ellipse_width, ellipse_height);
 
   //stroke(252, 162, 176); //RGB pink
   //line(50, 50, 50, 150);
@@ -53,7 +79,21 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   //line(150, 50, 50, 50);
 
 // across, down/up, across, downup
+
+//if statements
+//if(ellipse_width > 15){
+
+  //rect(100, 100, 100, 100)
+
 }
+//else{
+  //ellipse (100, 100, 100, 100)
+
+//}
+
+
+
+//}
 
 
 
